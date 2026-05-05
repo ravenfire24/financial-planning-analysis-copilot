daf8c799118256c0c7255cc0534a775d13c3bc3b
yonatesfag <ybtesfagiorgis@uh.edu>
2026-05-04 21:47:56 -0500

    i made changes

--- agent/llm.py
+++ agent/llm.py
@@ -1,17 +1,10 @@
-from transformers import AutoModelForSeq2SeqLM, AutoTokenizer, pipeline
+from transformers import pipeline
 from langchain_community.llms import HuggingFacePipeline
 
 def get_llm():
-    model_id = "google/flan-t5-base"
-
-    tokenizer = AutoTokenizer.from_pretrained(model_id)
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
-
     pipe = pipeline(
-        "text2text-generation",   
-        model=model,
-        tokenizer=tokenizer,
+        task="text-generation",
+        model="google/flan-t5-small",   
         max_new_tokens=256
     )
-
     return HuggingFacePipeline(pipeline=pipe)
